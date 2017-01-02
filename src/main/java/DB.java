49b97b2479038360840222760a32822ec7990055
markuslyconhold <ma223ik@student.lnu.se>
2017-01-02 02:36:02 +0100

    Initial commit

---
+++ src/main/java/DB.java
@@ -0,0 +1,5 @@
+/**
+ * Created by markuslyconhold on 02/01/17.
+ */
+public class DB {
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by markuslyconhold on 02/01/17.
+ */
+public class Main {
+    public static void main(String[]args){
+        System.out.println("test-test");
+    }
+}
