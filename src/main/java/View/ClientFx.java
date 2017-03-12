133c2cc9ff6e2ed7b1470b80dd9c677742dfb584
markuslyconhold <ma223ik@student.lnu.se>
2017-03-12 23:45:01 +0100

    Finished the first part of the clientFx class. Next step will be to implement the functions that makes it so that the information you insert in the text fields is then inserted into the database.

--- src/main/java/View/ClientFx.java
+++ src/main/java/View/ClientFx.java
@@ -2,6 +2,9 @@ package View;
 
 import javafx.application.Application;
 import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.*;
 
@@ -30,6 +33,34 @@ public class ClientFx extends Application {
         grid.setHgap(30);
         grid.setVgap(30);
 
+        Button submit = new Button("Submit");
+
+        Label nameLabel = new Label("Insert Name");
+        Label weightLabel = new Label("Insert Weight");
+        Label heightLabel = new Label("Insert Height");
+        Label ageLabel = new Label("Insert Age");
+
+        TextField nameField = new TextField();
+        TextField weightField = new TextField();
+        TextField heightField = new TextField();
+        TextField ageField = new TextField();
+
+
+        grid.add(nameLabel, 3,1 );
+        grid.add(weightLabel, 3, 2);
+        grid.add(heightLabel, 3, 3);
+        grid.add(ageLabel, 3, 4);
+
+        grid.add(nameField, 4, 1);
+        grid.add(weightField, 4, 2);
+        grid.add(heightField, 4, 3);
+        grid.add(ageField, 4, 4);
+
+
+
+        grid.add(submit, 4, 5);
+
+        root.getChildren().add(grid);
 
         stage.setScene(scene);
         stage.show();
